f59e7628dafee2e510355b1515cd59cb48002cd0
Renan Damascena Pereira <renandamascena@gmail.com>
2023-09-02 18:52:02 -0300

    fix: cases grid for mobile

--- src/components/Cases.jsx
+++ src/components/Cases.jsx
@@ -6,10 +6,20 @@ function Cases(){
                 <h1>Problemas complexos.<br/> Soluções <span>complexas</span> criativas_</h1>
                 <p>Confira nossos cases de sucesso que vão além do mundo juridico.</p>
             </div>
-            <div className='Case'></div>
-            <div className='Case'></div>
-            <div className='Case'></div>
-            <div className='Case'></div>
+            <div className='Case'>
+                <h2>Solução 1</h2>
+                <p>Repita comigo: Não vou esquecer de prestar atenção se está tudo alinhadinho. Não vou esquecer de prestar atenção se está tudo alinhado. Não vou esquecer de prestar atenção. Não esqueça do chat, ele está pouco visível na imagem acima mas no protótipo fica melhor.</p>
+            </div>
+            <div className='Case'>
+                <h2>Solução 2</h2>
+            </div>
+            <div className='Case'>
+                <h2>Solução 3</h2>
+            </div>
+            <div className='Case'>
+                <h2>Solução 4</h2>
+            </div>
+           
            
         </div>
     )

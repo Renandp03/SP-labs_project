9739e25504a9c2f63163d82ce9af73d3432eafdf
Renan Damascena Pereira <renandamascena@gmail.com>
2023-09-04 18:13:02 -0300

    update: contact form style

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,15 @@ function Contact(){
         <div className="Contact">
             <h1 className="Contact__Tittle">Não fique parado, fale conosco</h1>
             <div className='Contact__Form'>
-                <input className='Contact__Input' placeholder='Seu nome'/>
+                <input className='Contact__Input' placeholder='Seu nome' type='text'/>
+                <input className='Contact__Input' placeholder='Seu email' type='email'/>
+                <input className='Contact__Input' placeholder='Seu segmento' type='email'/>
+                <textarea className='Contact__Input--Big' placeholder='Fale um pouco sobre o seu negócio' cols="25" rows="5"></textarea>
+                <div className='Contact__Checklist'>
+                    <input type='checkbox' id='my_checkbox'/>
+                    <label>Declaro que conheço a Política de Privacidade e autorizo a utilização das minhas informações pelo SP Labs</label>
+                </div>
+                <button type='submit' className='Contact__Button'>enviar</button>
             </div>
         </div>
     )

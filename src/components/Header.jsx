be5b24b304cc69af4ba8f6b82c8ca3461a407bb5
Renan Damascena Pereira <renandamascena@gmail.com>
2023-09-05 20:41:11 -0300

    fix: Case button hover style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,8 @@ function Header(){
         <div className='Header'>
             <h1 className='Header__logo'>SP labs_</h1>
             <div className='Header__menu'>
-                <a className='Menu__a'>Cases</a>
-                <a className='Menu__a'>Contato</a>
+                <a className='Menu__Link'>Cases</a>
+                <a className='Menu__Link'>Contato</a>
             </div>
         </div>
     )

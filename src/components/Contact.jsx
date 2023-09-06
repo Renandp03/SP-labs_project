ab53619f92b777221469f1c3c87da6c9087ea2a1
Renan Damascena Pereira <renandamascena@gmail.com>
2023-09-05 22:15:39 -0300

    feat: contact decorator

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,6 +3,7 @@ import '../styles/components/Contact.sass'
 function Contact(){
     return(
         <div className="Contact">
+            <div className='Contact__Decorator'/>
             <h1 className="Contact__Tittle">Não fique parado, fale conosco</h1>
             <div className='Contact__Form'>
                 <input className='Contact__Input' placeholder='Seu nome' type='text'/>
